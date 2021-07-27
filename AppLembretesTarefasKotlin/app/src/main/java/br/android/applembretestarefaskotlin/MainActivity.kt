705c71e41cbd19824f774772e83dd9d67c5344d7
IsaqueAraujoProfissional <isaquearaujo.profissional@gmail.com>
2021-07-27 20:35:25 -0300

    Finalizando projeto base

--- AppLembretesTarefasKotlin/app/src/main/java/br/android/applembretestarefaskotlin/MainActivity.kt
+++ AppLembretesTarefasKotlin/app/src/main/java/br/android/applembretestarefaskotlin/MainActivity.kt
@@ -1,11 +1,65 @@
 package br.android.applembretestarefaskotlin
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import br.android.applembretestarefaskotlin.databinding.ActivityMainBinding
+import br.android.applembretestarefaskotlin.datasource.TaskDataSource
+import br.android.applembretestarefaskotlin.ui.AddTaskActivity
+import br.android.applembretestarefaskotlin.ui.TaskListAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val adapter by lazy { TaskListAdapter() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.rvTasks.adapter = adapter
+        updateList()
+
+        insertListeners()
+        //DATA STORE
+        //ROOM
     }
+
+    private fun insertListeners() {
+        binding.fab.setOnClickListener {
+            startActivityForResult(Intent(this, AddTaskActivity::class.java), CREATE_NEW_TASK)
+        }
+
+        adapter.listenerEdit = {
+            val intent = Intent(this, AddTaskActivity::class.java)
+            intent.putExtra(AddTaskActivity.TASK_ID, it.id)
+            startActivityForResult(intent, CREATE_NEW_TASK)
+        }
+
+        adapter.listenerDelete = {
+            TaskDataSource.deleteTask(it)
+            updateList()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CREATE_NEW_TASK && resultCode == Activity.RESULT_OK) updateList()
+    }
+
+    private fun updateList() {
+        val list = TaskDataSource.getList()
+        binding.includeEmpty.emptyState.visibility = if (list.isEmpty()) View.VISIBLE
+        else View.GONE
+
+        adapter.submitList(list)
+    }
+
+    companion object {
+        private const val CREATE_NEW_TASK = 1000
+    }
+
 }
